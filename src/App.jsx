2b2f53754fefd941cdf8d8fd2c1309548b03b8be
Ibrahim Ansari <iamibrahimansari@gmail.com>
2023-06-13 04:47:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Router>
+      <main>
+        <Routes>
+          <Route />
+        </Routes>
+      </main>
+    </Router>
+  )
+}
+
+export default App
